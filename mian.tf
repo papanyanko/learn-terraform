87b85fe417a7d666f9c1ae553ee5c9aa2ca900d0
papanyanko <bacct8979@gmail.com>
2024-04-30 18:19:58 +0900

    created an autoscaling cluster

--- mian.tf
+++ mian.tf
@@ -2,20 +2,33 @@ provider "aws" {
   region = "us-east-2"
 }
 
-resource "aws_instance" "example" {
-  ami = "ami-0fb653ca2d3203ac1"
+resource "aws_launch_configuration" "example" {
+  image_id = "ami-0fb653ca2d3203ac1"
   instance_type = "t2.micro"
-  vpc_security_group_ids = [ aws_security_group.instance.id ]
+  security_groups = [ aws_security_group.instance.id ]
 
   user_data = <<-EOF
                 #!/bin/bash
                 echo "Hello, World" > index.html
                 nohup busybox httpd -f -p ${var.server_port} &
                 EOF
-  user_data_replace_on_change = true
 
-  tags = {
-    Name = "terraform-example"
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = aws_launch_configuration.example.name
+  vpc_zone_identifier = data.aws_subnets.default.ids
+
+  min_size = 2
+  max_size = 10
+
+  tag {
+    key = "Name"
+    value = "terraform-asg-example"
+    propagate_at_launch = true
   }
 }
 
@@ -37,13 +50,19 @@ resource "aws_security_group" "instance" {
   ]
 }
 
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name = "vpc-id"
+    values = [ data.aws_vpc.default.id ]
+  }
+}
+
 variable "server_port" {
   description = "The port the server will use or HTTP requests"
   type = number
   default = 8080
-}
-
-output "public_ip" {
-  description = "The public IP address of the web server"
-  value = aws_instance.example.public_ip
 }
