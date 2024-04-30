7a6a4b0a698471b5023c72b52ebf417b1cb1771c
papanyanko <bacct8979@gmail.com>
2024-04-30 21:57:46 +0900

    added load balancer to auto scaling group

--- mian.tf
+++ mian.tf
@@ -22,6 +22,9 @@ resource "aws_autoscaling_group" "example" {
   launch_configuration = aws_launch_configuration.example.name
   vpc_zone_identifier = data.aws_subnets.default.ids
 
+  target_group_arns = [ aws_lb_target_group.asg.arn ]
+  health_check_type = "ELB"
+
   min_size = 2
   max_size = 10
 
@@ -29,6 +32,61 @@ resource "aws_autoscaling_group" "example" {
     key = "Name"
     value = "terraform-asg-example"
     propagate_at_launch = true
+  }
+}
+
+resource "aws_lb" "example" {
+  name = "terraform-asg-example"
+  load_balancer_type = "application"
+  subnets = data.aws_subnets.default.ids
+  security_groups = [ aws_security_group.alb.id ]
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.example.arn
+  port = 80
+  protocol = "HTTP"
+
+  default_action {
+    type = "fixed-response"
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "404: page not found"
+      status_code = 404
+    }
+  }
+}
+
+resource "aws_lb_listener_rule" "asg" {
+  listener_arn = aws_lb_listener.http.arn
+  priority = 100
+
+  condition {
+    path_pattern {
+      values = [ "*" ]
+    }
+  }
+
+  action {
+    type = "forward"
+    target_group_arn = aws_lb_target_group.asg.arn
+  }
+}
+
+resource "aws_lb_target_group" "asg" {
+  name = "terraform-asg-example"
+  port = var.server_port
+  protocol = "HTTP"
+  vpc_id = data.aws_vpc.default.id
+
+  health_check {
+    path = "/"
+    protocol = "HTTP"
+    matcher = "200"
+    interval = 15
+    timeout = 3
+    healthy_threshold = 2
+    unhealthy_threshold = 2
   }
 }
 
@@ -50,6 +108,38 @@ resource "aws_security_group" "instance" {
   ]
 }
 
+resource "aws_security_group" "alb" {
+  name = "terraform-example-alb"
+
+  ingress = [
+    {
+      description = "alb ingress"
+      from_port = 80
+      to_port = 80
+      protocol = "tcp"
+      cidr_blocks = [ "0.0.0.0/0" ]
+      ipv6_cidr_blocks = [  ]
+      prefix_list_ids = [  ]
+      security_groups = [  ]
+      self = false
+    }
+  ]
+
+  egress = [ 
+    {
+      description = "alb ingress"
+      from_port = 0
+      to_port = 0
+      protocol = "-1"
+      cidr_blocks = [ "0.0.0.0/0" ]
+      ipv6_cidr_blocks = [  ]
+      prefix_list_ids = [  ]
+      security_groups = [  ]
+      self = false
+    }
+   ]
+}
+
 data "aws_vpc" "default" {
   default = true
 }
@@ -65,4 +155,9 @@ variable "server_port" {
   description = "The port the server will use or HTTP requests"
   type = number
   default = 8080
+}
+
+output "alb_dns_name" {
+  value = aws_lb.example.dns_name
+  description = "The domain name of the load balancer"
 }
