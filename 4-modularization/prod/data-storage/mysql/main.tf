b82ed5ea70a8e42bf7e99f00d925252ed2db0558
papanyanko <bacct8979@gmail.com>
2024-05-04 20:13:00 +0900

    created production module

---
+++ 4-modularization/prod/data-storage/mysql/main.tf
@@ -0,0 +1,22 @@
+terraform {
+  backend "s3" {
+    bucket = "terraform-state-31415"
+    key = "prod/data-storage/mysql/terraform.tfstate"
+    region = "us-east-2"
+    dynamodb_table = "terraform-up-and-running-locks"
+    encrypt = true
+  }
+}
+
+provider "aws" {
+  region = "us-east-2"
+}
+
+module "data_storage" {
+  source = "../../../modules/data-storage/mysql"
+
+  db_name = "dbprod"
+  instance_class = "db.t3.micro"
+  db_password = var.db_password
+  db_username = var.db_username
+}
--- 4-modularization/prod/services/webserver-cluster/main.tf
+++ 4-modularization/prod/services/webserver-cluster/main.tf
@@ -1,3 +1,13 @@
+terraform {
+  backend "s3" {
+    bucket = "terraform-state-31415"
+    key = "prod/services/webserver-cluster/terraform.tfstate"
+    region = "us-east-2"
+    dynamodb_table = "terraform-up-and-running-locks"
+    encrypt = true
+  }
+}
+
 provider "aws" {
   region = "us-east-2"
 }
@@ -8,11 +18,4 @@ module "webserver_cluster" {
   cluster_name = "webservers-prod"
   db_remote_state_bucket = "terraform-state-31415"
   db_remote_state_key = "prod/data-storage/mysql/terraform.tfstate"
-}
-
-module "data_storage" {
-  source = "../../../modules/data-storage/mysql"
-
-  db_password = "????"
-  db_username = "????"
 }
