f48386ff74f31c3cf9f035a7a36f417483d7c464
papanyanko <bacct8979@gmail.com>
2024-05-04 20:23:52 +0900

    set up auto scaling group

--- 4-modularization/prod/services/webserver-cluster/main.tf
+++ 4-modularization/prod/services/webserver-cluster/main.tf
@@ -19,3 +19,26 @@ module "webserver_cluster" {
   db_remote_state_bucket = "terraform-state-31415"
   db_remote_state_key = "prod/data-storage/mysql/terraform.tfstate"
 }
+
+
+resource "aws_autoscaling_schedule" "scale_out_during_business_hours" {
+  scheduled_action_name = "scale_out_during_business_hours"
+  min_size = 2
+  max_size = 10
+  desired_capacity = 10
+  time_zone = "Asia/Tokyo"
+  recurrence = "0 9 * * *"
+
+  autoscaling_group_name = module.webserver_cluster.asg_name
+}
+
+resource "aws_autoscaling_schedule" "scale_in_at_night" {
+  scheduled_action_name = "scale_in_at_nigh"
+  min_size = 2
+  max_size = 10
+  desired_capacity = 2
+  time_zone = "Asia/Tokyo"
+  recurrence = "0 17 * * *"
+
+  autoscaling_group_name = module.webserver_cluster.asg_name
+}
